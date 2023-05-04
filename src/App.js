5430db9bbdf544c906b3cdf4110adbf17a07bc84
swaraj bachu <rajswaraj.r@gmail.com>
2023-05-04 23:35:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { motion } from 'framer-motion'
+import { useState } from 'react';
+
+
+const EmojiThrower = ({ emoji  }) => {
+  const randomX = Math.random() * (window.innerWidth - 100)
+  return (
+      <motion.div
+          z-index={100}
+          initial={{ y: '100vh', x: randomX, opacity: 1, position: 'absolute' }}
+          animate={{ opacity: 0, y: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 2 }}
+      >
+          <p style={{ fontSize: '40px' }}>{emoji}</p>
+      </motion.div>
+  )
+}
+
 
 function App() {
+  const [emote, setEmote] = useState([])
+  const handleClick = () => { setEmote([...emote, 1])}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+      <div style={
+              { display: 'flex', justifyContent: 'flex-end' }
+          }>
+              <button onMouseOver={handleClick}>
+                  Money Money 
+              </button>
+          </div>
+              {emote.map((item, i) => {
+                  return <EmojiThrower emoji={'💰'} key={i} />
+              })}
+      </div>
+  )
 }
 
 export default App;
